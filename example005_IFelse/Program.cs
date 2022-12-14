5c99f6d1de47ae07e74296c26d58e74e8122578a
akrchnv <52063796+akrchnv@users.noreply.github.com>
2022-12-14 22:52:40 +0300

    added if else operator with greeting

---
+++ example005_IFelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("УРА МАША");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
